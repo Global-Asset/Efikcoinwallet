3586f73ab49835b2ef4b31e35414b62a99f9c821
Efikcoin wallet <Support@efikcoin.xyz>
2026-07-05 07:42:33 +0200

    Add initial app configuration for EfikcoinWallet

---
+++ app.config.js
@@ -0,0 +1,23 @@
+{
+  "expo": {
+    "name": "EfikcoinWallet",
+    "slug": "efikcoin-wallet-efc",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "scheme": "efikcoinwallet",
+    "ios": {
+      "bundleIdentifier": "com.efikcoin.wallet",
+      "buildNumber": "1.0.0"
+    },
+    "android": {
+      "package": "com.efikcoin.wallet",
+      "versionCode": 1
+    },
+    "extra": {
+      "eas": {
+        "projectId": "89b205bc-775a-45e5-9f32-591ad3ced271"
+      }
+    }
+  }
+}
